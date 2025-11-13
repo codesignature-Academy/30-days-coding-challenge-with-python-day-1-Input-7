fc98105be5189de9da2c29aa3825855f97dc6528
input-7 <xmylevin@gmail.com>
2025-11-13 21:53:48 +0100

    my first commit

---
+++ day1/assignment.py
@@ -0,0 +1,5 @@
+name = input("Enter your name: ")
+hobbies = input("What are your hobbies? ")
+hobbies_duration = input("How long have you been engaged in these hobbies? ")
+
+print(f"My name is {name}. My hobbies are {hobbies}, \n\t and I have been engaged in these hobbies for {hobbies_duration}.")
