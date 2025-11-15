82f256785242e0a2daff7cce179ec338864277eb
input-7 <xmylevin@gmail.com>
2025-11-15 22:50:55 +0100

    my third-day commit

---
+++ day3/assignment.py
@@ -0,0 +1,21 @@
+# # Task: Student Admission Checker
+
+departments = {"medicine", "engineering", "law", "bussiness"}
+for department in departments:
+    if department == "medicine":
+        cut_off = 250
+if department == "medicine":
+        cut_off = 250
+if department == "medicine":
+        cut_off = 250
+if department == "medicine":
+        cut_off = 250
+
+print("Welcome To University of Techrise")
+department = (input("Enter your department: "))
+score = int(input("Enter your Jamb score: "))
+
+if score >= cut_off:
+    print("Congratulations, you are admitted into <department>!" )
+else:
+    print("Sorry, you were not admitted.")
---
+++ day3/class.py
@@ -0,0 +1,29 @@
+# # Task 1 - Ask user for a number → print whether it's odd or even.
+# Num = int(input("Enter a number: "))
+# oddcheck = Num % 2
+# if oddcheck == 0:
+#     print("this is an even number ")
+# else:
+#     print("this is an odd number")
+
+# # Task 2 — Voter Eligibility
+# name = (input("Enter your name: "))
+# age = int(input("Enter your age: "))
+# if age >= 18: 
+#     print("You are eligible to vote ")
+# else:
+#     print("You are NOT eligible to vote ")
+
+# Task 3 — Grade Checker
+
+score = int(input("Enter your score: "))
+if score >= 70:
+    print("A")
+elif score >= 50:
+    print("B")
+elif score >= 45:
+    print("C")
+elif score >= 40:
+    print("D")  
+else:
+    print("F")
